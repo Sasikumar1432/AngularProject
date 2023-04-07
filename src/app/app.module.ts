20ad71add2396589d45b096d213c7a16bb9600f6
Pothu sasikumarreddy <pothusasikumarreddy143@gmail.com>
2023-04-07 16:36:31 +0530

    New components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
